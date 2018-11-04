3d1b02cb292d0421facd83c4acb0793ae76fb8b1
GrOOvKe <tim@groovke.be>
2018-11-04 02:10:01 +0100

    Add project files.

---
+++ DicordBot.app/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DicordBot.app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
